a24f456ce452a211545f2f6e4234d20b6a1b5fd5
Jesús García Sánchez <gsus@go.ugr.es>
2025-11-07 13:36:31 +0100

    create_enviroment updated for non interactive installation

--- installation/scripts/create_enviroment.sh
+++ installation/scripts/create_enviroment.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-conda create --name lafragua python=3.12
+set -e
+
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main || true
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r || true
+
+conda create --name lafragua python=3.12 -y
 bash -i -c "conda init && conda activate lafragua && pip install -r ./installation/requirements.txt"
